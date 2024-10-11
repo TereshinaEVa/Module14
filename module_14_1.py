68eaa7dabfecb3815ecd5e22d83768ec27a6a1d9
TereshinaEVa <tereshina.eva@gmail.com>
2024-10-11 21:58:46 +0300

    Создание БД, добавление, выбор и удаление эл-тов

--- module_14_1.py
+++ module_14_1.py
@@ -14,11 +14,16 @@ balance INTEGER NON NULL
 ''')
 
 # for i in range(1, 11):
-#     cursor.execute("INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)", (f'User{i}', f'example{i}@gmail.com', i*10, 1000))
+#      cursor.execute("INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)", (f'User{i}', f'example{i}@gmail.com', i*10, 1000))
 
 #cursor.execute("UPDATE Users SET balance = ? WHERE id%2 = ?", (500, 1))
+#cursor.execute("DELETE FROM Users WHERE id%3 = ?", (1,))
 
-cursor.execute("DELETE FROM Users WHERE balance = ?", (1000,))
+cursor.execute("SELECT username, email, age, balance FROM Users WHERE age != ?", (60,))
+#Имя: <username> | Почта: <email> | Возраст: <age> | Баланс: <balance>
+users = cursor.fetchall()
+for user, em, age, balance in users:
+    print(f"Имя: {user} | Почта: {em} | Возраст: {age} | Баланс: {balance}")
 
 connection.commit()
 connection.close()
